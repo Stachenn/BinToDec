f643cdc1de15696bbe300af00f445275f930f716
Stachenn <155658707+Stachenn@users.noreply.github.com>
2024-02-10 00:07:24 +0100

    Main file
    0️⃣1️⃣0️⃣1️⃣0️⃣1️⃣0️⃣0️⃣0️⃣1️⃣

---
+++ main.py
@@ -0,0 +1,106 @@
+# Pozyskanie liczby
+def decToBin():
+    while True:
+        userBin = input("Type ur dec num : ")
+    
+        try:
+            userBinInt = int(userBin)
+        except ValueError:
+            print("Ur num is not int")
+        else:
+            break
+
+    userBinTable = []
+    binTable = []
+    j = 1
+    cursor = len(userBinTable)-1
+    readyString = ""
+    m = 0
+
+    while True:
+        binTable.append(j)
+        if userBinInt <= j:
+            break
+        j = j * 2
+#for i in binTable:
+#       print(i)
+    while True: 
+        try:
+            # Debugowanie znowu print("\n",binTable[cursor])
+#if cursor <= -1:
+#               break
+            if binTable[cursor] <= userBinInt:
+                readyString += "1"
+                # Nie potrzebne instrukcja do debugowania ;) 
+    #print(binTable[cursor], "|", userBinInt, "|1")
+                userBinInt-=binTable[cursor]
+            else:
+                # To samo 
+#print(binTable[cursor], "|", userBinInt, "| 0")
+                readyString+= "0"
+            cursor-=1
+        except IndexError:
+            break
+    return f"Result : {str(readyString)}\n\n\n\n"
+
+def binToDec():
+    while True:
+        userBin = input("Type ur bin num : ")
+    
+        try:
+            userBinInt = int(userBin)
+        except ValueError:
+            print("Ur num is not int")
+        else:
+            break
+    j = 1
+    cursorForDecompite = len(userBin)-1
+    m = len(userBin)-1
+    userBinTable = []
+    binTable = []
+    readyString = 0
+    cursor = 0
+
+    while True:
+        if m == -1:
+            break
+        binTable.append(j)
+        userBinTable.append(int(userBin[m]))
+        j = j * 2
+        m -= 1
+    while True:
+        try:
+#print("ff")
+#print(type(userBinTable[cursor]), "|", "1")
+#print(userBin[cursor])
+#print(userBinTable[cursor])
+            if userBin[cursor] == "1":
+                readyString += int(binTable[cursorForDecompite])
+        #print("bin=",int(binTable[cursorForDecompite]))
+        #print("ready=",readyString,"\n\n")
+            if cursor == len(userBin)-1:
+                break
+            cursor += 1
+            cursorForDecompite -= 1
+        except IndexError:
+            break
+#print(readyString)
+    return f"Result : {str(readyString)}\n\n\n\n"
+
+def main():
+    while True:
+        print("\n| [C]Stachenn all rights reserved | \n\nWhat u wanna do? \n 1 - Bin to dec \n 2 - Dec to bin \n 3 - Exit (pls dont do it u can be there forever)")
+        choice = input()
+        if choice == "1":
+            print(binToDec())
+            break
+        if choice == "2":
+            print(decToBin())
+            break
+        if choice == "3":
+            print("Thank for using Stacehnn software goodbye, have a nice day/night!")
+            exit(0)
+        else:
+            print("Ur choice is not in option")
+while True:
+    main()
